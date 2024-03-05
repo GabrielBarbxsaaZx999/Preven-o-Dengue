3a2b5de23dbaccec50e697c4c242e872ae933aa3
GABRIEL SO$A <125594732+GabrielBarbxsaaZx999@users.noreply.github.com>
2024-03-05 13:57:51 -0400

    Add files via upload

---
+++ homeTela/home.js
@@ -0,0 +1,13 @@
+// header.js
+document.getElementById('header-container').innerHTML = `
+    <header>
+        <h1>Seu Cabeçalho</h1>
+    </header>
+`;
+
+// footer.js
+document.getElementById('footer-container').innerHTML = `
+    <footer>
+        <p>Seu Rodapé &copy; 2024</p>
+    </footer>
+`;
